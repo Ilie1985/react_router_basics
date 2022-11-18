b682f46b0aa862a97e92a114fcc16ac64c98fbe8
Ilie1985 <ilie.marian32@yahoo.com>
2022-11-18 11:39:14 +0000

    dashboardjs

---
+++ src/Dashboard.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+
+
+
+
+const Dashboard = () => {
+  return (
+    <section className='section'>
+      <h4>Dashboard</h4>
+    </section>
+  );
+};
+export default Dashboard;
